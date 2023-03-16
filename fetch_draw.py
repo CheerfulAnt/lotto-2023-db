5a99522934fef0b23cd1272fc9ecbc5c41115e8a
CheerfulAnt <CheerfulAnt@outlook.com>
2023-03-16 22:02:13 +0100

    New function chunks_generator().

--- fetch_draw.py
+++ fetch_draw.py
@@ -51,40 +51,73 @@ def check_last_draw(game_type=cfg.config['DEFAULT_GAME']):
     return last_game_id
 
 
+def chunks_generator(last, chunk):
+
+    chunks_list = list()
+
+    if last <= chunk:
+        chunks_list.append((1, last, last))
+        return chunks_list
+
+    def chungen(last, chunk):
+
+        aux_list = list()
+        rest = last % chunk
+        base = last - rest
+        index_qty = int(base / chunk)
+
+        if len(chunks_list):
+            last_hop = chunks_list[-1][-1]
+        else:
+            last_hop = 0
+
+        hop = index_qty * chunk
+        chunk_qty = int((hop - last_hop) / chunk)
+        for _ in range(chunk_qty):
+            hop = index_qty * chunk
+            aux_list.insert(0, (index_qty, chunk, hop))
+            index_qty -= 1
+        chunks_list.extend(aux_list)
+        aux_list.clear()
+        return rest
+
+    rest = chungen(last=last, chunk=chunk)
+
+    dividers = list()
+
+    for i in range(1, chunk + 1):
+        if chunk % i == 0:
+            dividers.append(i)
+
+    dividers.reverse()
+    dividers_qty = len(dividers)
+
+    for x in range(dividers_qty):
+        if x + 1 == dividers_qty:
+            break
+
+        if dividers[x + 1] <= rest < dividers[x] and rest > 0:
+            chunk = dividers[x + 1]
+            rest = chungen(last=last, chunk=chunk)
+
+    return chunks_list
+
+
+print(chunks_generator(6856, 1000))
+
+
 # fetch draws
 
-db_obj = dbq.DB()
-
-for item in cfg.game_type_list:
-    print(item)
-    last_game_id = check_last_draw(game_type=item)
-    print(cfg.game_dict[item][0])
-    last_game_id_db = db_obj.check_last_draw_db(cfg.game_dict[item][0])
-    chunk_size = cfg.config['CHUNK_SIZE']
-    print('last id ', last_game_id)
-    print('last id db ', last_game_id_db)
-
-    if last_game_id_db is None:
-        if last_game_id / chunk_size <= 1:
-            print('None by once: ', last_game_id)
-            # fetch all by once
-            # index = last_game_id
-            # size = last_game_id
-        else:
-            iter_cnt = last_game_id // chunk_size
-            index = last_game_id
-            # size = chunk_size
-            for i in range(iter_cnt + 1):
-                #time.sleep(2)
-                print(f'index={index}&size={chunk_size}')
-                if index < chunk_size:
-                    chunk_size = index
-                else:
-                    index -= chunk_size
-
-
-
-
-    # if last_game_id > last_game_id_db:
-    #     pass
-
+# db_obj = dbq.DB()
+#
+# for item in cfg.game_type_list:
+#     print(item)
+#     last_game_id = check_last_draw(game_type=item)
+#     print(cfg.game_dict[item][0])
+#     last_game_id_db = db_obj.check_last_draw_db(cfg.game_dict[item][0])
+#     chunk_size = cfg.config['CHUNK_SIZE']
+#     print('last id ', last_game_id)
+#     print('last id db ', last_game_id_db)
+#     time.sleep()
+#     if last_game_id_db is None:
+#         pass
