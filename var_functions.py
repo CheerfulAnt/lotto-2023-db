cc21e72864c8958c5328c0d80816694c16c33335
CheerfulAnt <CheerfulAnt@outlook.com>
2023-04-26 21:11:50 +0200

    First commit.

---
+++ var_functions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Created By: CheerfulAnt (Github account holder) <CheerfulAnt@outlook.com>
+# License: MIT License.
+# ---------------------------------------------------------------------------
+""" This script is part of the lotto-2023-db project. """
+# ---------------------------------------------------------------------------
+# Imports from lotto-2023-db
+# ---------------------------------------------------------------------------
+import cfg
+import event_report
+import db_query as dbq
+# ---------------------------------------------------------------------------
+# Imports
+# -----------------------------------------------------------------------
+
+
+def cron_generate():
+	pass
+
+def csv_dump():
+	pass
